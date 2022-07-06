b2d9a23ab3d43099a2360fb6401cb87257615d2d
Aswathy Saji <aswathy2saji@gmail.com>
2022-07-06 14:02:45 +0530

    updated navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './NavBar.css'
+import { useState,useEffect } from 'react';
 
 function NavBar() {
 
+  const [ show,handleShow ] = useState(false)
+
+  useEffect(() => {
+     window.addEventListener('scroll',()=>{
+      if(window.scrollY > 100){
+        handleShow(true);
+      }
+      else{
+        handleShow(false);
+      }
+     });
+     return()=>{
+      window.removeEventListener('scroll',null);
+     };
+  }, []);
+  
+  
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${show && 'nav_black' } `}/*{`navbar ${show && "nav_black" }` }*/>
         <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="netflix-logo" />
         <img className='avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="avatar" />
     </div>
